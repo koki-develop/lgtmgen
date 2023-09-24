573b20d35ecdba8d62e00a1e36b43cd015f0d100
koki-develop <kou.pg.0131@gmail.com>
2023-09-24 11:15:01 +0900

    Add AWS Lambda support to server command

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -1,20 +1,28 @@
 package cmd
 
 import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/koki-develop/lgtmgen/backend/internal/api"
 	"github.com/spf13/cobra"
+	"golang.org/x/net/context"
+)
+
+var (
+	flagServeLambda bool // --lambda
 )
 
 var serveCmd = &cobra.Command{
 	Use: "serve",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		r := gin.Default()
+		r := api.NewEngine()
 
-		r.GET("/h", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"status": "ok",
-			})
-		})
+		if flagServeLambda {
+			lambda.Start(serveLambdaHandler(r))
+			return nil
+		}
 
 		if err := r.Run(":8080"); err != nil {
 			return err
@@ -24,6 +32,16 @@ var serveCmd = &cobra.Command{
 	},
 }
 
+type lambdaHandler func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
+
+func serveLambdaHandler(e *gin.Engine) lambdaHandler {
+	return func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		ginLambda := ginadapter.New(e)
+		return ginLambda.ProxyWithContext(ctx, req)
+	}
+}
+
 func init() {
 	rootCmd.AddCommand(serveCmd)
+	serveCmd.Flags().BoolVar(&flagServeLambda, "lambda", false, "Run as lambda")
 }
