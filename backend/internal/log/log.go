fae2dc64792e904c639f89d185730f0995d85e63
koki-develop <kou.pg.0131@gmail.com>
2023-09-24 23:20:15 +0900

    Create log package

---
+++ backend/internal/log/log.go
@@ -0,0 +1,24 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+var (
+	logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+)
+
+func Warn(ctx context.Context, msg string, args ...interface{}) {
+	logger.WarnContext(ctx, msg, args...)
+}
+
+func Info(ctx context.Context, msg string, args ...interface{}) {
+	logger.InfoContext(ctx, msg, args...)
+}
+
+func Error(ctx context.Context, msg string, err error, args ...interface{}) {
+	logger.ErrorContext(ctx, msg, append(args, "error", err, "trace", fmt.Sprintf("%+v", err))...)
+}
