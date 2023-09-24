93bb156b84ec98a5d49671f84e1a8e7f549d74d8
koki-develop <kou.pg.0131@gmail.com>
2023-09-24 21:37:53 +0900

    Add LGTMStatus

--- backend/internal/models/lgtm.go
+++ backend/internal/models/lgtm.go
@@ -3,9 +3,16 @@ package models
 import "time"
 
 type LGTM struct {
-	ID        string    `json:"id" dynamodbav:"id"`
-	Status    string    `json:"-"  dynamodbav:"status"`
-	CreatedAt time.Time `json:"-"  dynamodbav:"created_at"`
+	ID        string     `json:"id" dynamodbav:"id"`
+	Status    LGTMStatus `json:"-"  dynamodbav:"status"`
+	CreatedAt time.Time  `json:"-"  dynamodbav:"created_at"`
 }
 
 type LGTMs []*LGTM
+
+type LGTMStatus string
+
+const (
+	LGTMStatusOK      LGTMStatus = "ok"
+	LGTMStatusPending LGTMStatus = "pending"
+)
