4db5bd0d73a4570617f65026e4bb34085c46e0b1
koki-develop <kou.pg.0131@gmail.com>
2023-09-24 19:03:27 +0900

    Add DynamoDB table creation to init script

--- localstack/ready.d/init.sh
+++ localstack/ready.d/init.sh
@@ -2,7 +2,23 @@
 
 set -euo pipefail
 
+readonly RESOURCE_PREFIX="lgtmgen-local"
+
 # Create Public S3 Bucket
 awslocal s3api create-bucket \
-  --bucket lgtmgen-local-images \
+  --bucket ${RESOURCE_PREFIX}-images \
   --acl public-read
+
+# Create DynamoDB Tables
+awslocal dynamodb create-table \
+  --table-name ${RESOURCE_PREFIX}-lgtms \
+  --key-schema \
+    AttributeName=id,KeyType=HASH \
+    AttributeName=created_at,KeyType=RANGE \
+  --attribute-definitions \
+    AttributeName=id,AttributeType=S \
+    AttributeName=created_at,AttributeType=S \
+    AttributeName=status,AttributeType=S \
+  --global-secondary-indexes \
+    IndexName=index_by_status,KeySchema=["{AttributeName=status,KeyType=HASH}","{AttributeName=created_at,KeyType=RANGE}"],Projection="{ProjectionType=ALL}" \
+  --billing-mode PAY_PER_REQUEST
