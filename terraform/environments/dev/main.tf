8f9046edd15d498dcc09037d0e5b196d82663d0a
koki-develop <kou.pg.0131@gmail.com>
2023-09-25 08:17:16 +0900

    Create images bucket

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -2,3 +2,9 @@ module "github_actions" {
   source = "../../modules/aws/github_actions"
   name   = local.name
 }
+
+module "s3_images" {
+  source = "../../modules/aws/s3"
+  name   = local.name
+  bucket = "images"
+}
---
+++ terraform/modules/aws/s3/main.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.name}-${var.bucket}"
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
