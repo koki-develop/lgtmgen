c1289ec20d6e994353d95de9aabc093e3c73c033
koki-develop <kou.pg.0131@gmail.com>
2023-09-24 09:36:52 +0900

    Add /h endpoint to serveCmd in serve.go

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -1,15 +1,25 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 )
 
 var serveCmd = &cobra.Command{
 	Use: "serve",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("serve called")
+		r := gin.Default()
+
+		r.GET("/h", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"status": "ok",
+			})
+		})
+
+		if err := r.Run(":8080"); err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
