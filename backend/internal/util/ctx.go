ba5a3b86a2d3ed419ca0f7b002bfcf566883aa34
koki-develop <kou.pg.0131@gmail.com>
2023-09-25 14:50:02 +0900

    Remove log

--- backend/internal/util/ctx.go
+++ backend/internal/util/ctx.go
@@ -3,7 +3,6 @@ package util
 import (
 	"github.com/awslabs/aws-lambda-go-api-proxy/core"
 	"github.com/gin-gonic/gin"
-	"github.com/koki-develop/lgtmgen/backend/internal/log"
 )
 
 func GetClientIPFromContext(ctx *gin.Context) string {
@@ -11,10 +10,8 @@ func GetClientIPFromContext(ctx *gin.Context) string {
 
 	apictx, ok := core.GetAPIGatewayContextFromContext(ctx.Request.Context())
 	if ok {
-		log.Info(ctx, "api gateway context found")
 		ip = apictx.Identity.SourceIP
 	} else {
-		log.Info(ctx, "no api gateway context")
 		ip = ctx.ClientIP()
 	}
 
