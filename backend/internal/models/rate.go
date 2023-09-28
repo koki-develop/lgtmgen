6818187e6eba811da3163f3322d336a12a31ee26
koki-develop <kou.pg.0131@gmail.com>
2023-09-28 18:47:26 +0900

    Add Rate model

---
+++ backend/internal/models/rate.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Rate struct {
+	IP      string    `json:"ip"       dynamodbav:"ip"`
+	Count   int       `json:"count"    dynamodbav:"count"`
+	ResetAt time.Time `json:"reset_at" dynamodbav:"reset_at"`
+}
+
+func (r *Rate) Expired() bool {
+	return r.ResetAt.Before(time.Now())
+}
+
+func (r *Rate) LimitReached(limit int) bool {
+	return r.Count >= limit
+}
