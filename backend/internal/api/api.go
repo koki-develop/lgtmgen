9e706a16c57dd9979b3f42c1ba27b8644ade2d22
koki-develop <kou.pg.0131@gmail.com>
2023-09-28 18:56:13 +0900

    Add RateLimiter

--- backend/internal/api/api.go
+++ backend/internal/api/api.go
@@ -5,11 +5,19 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/koki-develop/lgtmgen/backend/internal/log"
+	"github.com/koki-develop/lgtmgen/backend/internal/middleware"
+	"github.com/koki-develop/lgtmgen/backend/internal/repo"
 	"github.com/koki-develop/lgtmgen/backend/internal/service"
+	"github.com/pkg/errors"
 )
 
 func NewEngine(ctx context.Context) (*gin.Engine, error) {
-	svc, err := service.New(ctx)
+	r, err := repo.New(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to initialize repository")
+	}
+
+	svc, err := service.New(ctx, r)
 	if err != nil {
 		return nil, err
 	}
@@ -17,13 +25,14 @@ func NewEngine(ctx context.Context) (*gin.Engine, error) {
 	e := gin.New()
 	e.Use(log.Middleware)
 	e.Use(gin.Recovery())
+	rl := middleware.NewRateLimitter(r)
 
 	e.GET("/h", svc.HealthCheck)
 
 	v1 := e.Group("/v1")
 	{
 		v1.GET("/lgtms", svc.ListLGTMs)
-		v1.POST("/lgtms", svc.CreateLGTM)
+		v1.POST("/lgtms", rl.Apply(100), svc.CreateLGTM)
 
 		v1.POST("/reports", svc.CreateReport)
 
---
+++ backend/internal/middleware/rate.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/koki-develop/lgtmgen/backend/internal/log"
+	"github.com/koki-develop/lgtmgen/backend/internal/repo"
+	"github.com/koki-develop/lgtmgen/backend/internal/service"
+	"github.com/koki-develop/lgtmgen/backend/internal/util"
+)
+
+type RateLimitter struct {
+	repo *repo.Repository
+}
+
+func NewRateLimitter(r *repo.Repository) *RateLimitter {
+	return &RateLimitter{
+		repo: r,
+	}
+}
+
+func (m *RateLimitter) Apply(limit int) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ip := util.GetClientIPFromContext(ctx)
+		rate, err := m.repo.FindRate(ctx, ip)
+		if err != nil {
+			log.Error(ctx, "failed to find rate", err)
+		} else {
+			if rate != nil && rate.LimitReached(limit) {
+				log.Info(ctx, "rate limit reached", "rate", rate)
+				ctx.JSON(http.StatusTooManyRequests, gin.H{"code": service.ErrCodeRateLimitReached})
+				ctx.Abort()
+				return
+			}
+			if err := m.repo.IncrementRate(ctx, ip); err != nil {
+				log.Error(ctx, "failed to increment rate", err)
+			}
+		}
+
+		ctx.Next()
+	}
+}
