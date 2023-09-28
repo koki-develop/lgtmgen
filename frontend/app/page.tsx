dfca437df647b00e23bc96991b30d6cd1d40cd12
koki-develop <kou.pg.0131@gmail.com>
2023-09-29 08:51:27 +0900

    Add API call to Home function in page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <main>hello world</main>;
+import { api } from "@/lib/api";
+
+export default async function Home() {
+  const { data: lgtms } = await api.v1.lgtmsList({ limit: 20 });
+
+  return (
+    <main>
+      <ul>
+        {lgtms.map((lgtm) => (
+          <li key={lgtm.id}>{lgtm.id}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
