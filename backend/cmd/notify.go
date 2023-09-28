f2bb31fd6186af29891cec513bced0e62c114c7c
koki-develop <kou.pg.0131@gmail.com>
2023-09-28 13:20:04 +0900

    Add LGTM and report options to notify command

--- backend/cmd/notify.go
+++ backend/cmd/notify.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/cockroachdb/errors"
@@ -13,6 +15,8 @@ import (
 var (
 	flagNotifyLambda bool // --lambda
 	flagNotifyDebug  bool // --debug
+	flagNotifyLGTM   bool // --lgtm
+	flagNotifyReport bool // --report
 )
 
 var notifyCmd = &cobra.Command{
@@ -30,17 +34,41 @@ var notifyCmd = &cobra.Command{
 			if err != nil {
 				return errors.Wrap(err, "failed to create service")
 			}
+
+			var body map[string]interface{}
+			switch {
+			case flagNotifyLGTM:
+				body = map[string]interface{}{
+					"type": "lgtm_created",
+					"lgtm_created": map[string]interface{}{
+						"lgtm": map[string]interface{}{
+							"id": "00000000-0000-0000-0000-000000000000",
+						},
+						"source":    "https://example.com",
+						"client_ip": "127.0.0.1",
+					},
+				}
+			case flagNotifyReport:
+				body = map[string]interface{}{
+					"type": "report_created",
+					"report_created": map[string]interface{}{
+						"report": map[string]interface{}{
+							"id":   "00000000-0000-0000-0000-000000000000",
+							"type": "other",
+							"text": "Hello, World!",
+						},
+					},
+				}
+			default:
+				return errors.New("required --lgtm or --report")
+			}
+
+			p, err := json.Marshal(body)
+			if err != nil {
+				return errors.Wrap(err, "failed to marshal body")
+			}
 			err = svc.Notify(ctx, &events.SQSEvent{
-				Records: []events.SQSMessage{
-					{Body: `{
-            "type": "lgtm_created",
-            "lgtm_created": {
-              "lgtm": {"id": "00000000-0000-0000-0000-000000000000"},
-              "source": "https://example.com",
-              "client_ip": "127.0.0.1"
-            }
-          }`},
-				},
+				Records: []events.SQSMessage{{Body: string(p)}},
 			})
 			if err != nil {
 				return errors.Wrap(err, "failed to notify")
@@ -70,4 +98,7 @@ func init() {
 	rootCmd.AddCommand(notifyCmd)
 	notifyCmd.Flags().BoolVar(&flagNotifyLambda, "lambda", false, "Run as lambda")
 	notifyCmd.Flags().BoolVar(&flagNotifyDebug, "debug", false, "Run as debug")
+	notifyCmd.Flags().BoolVar(&flagNotifyLGTM, "lgtm", false, "Type is lgtm")
+	notifyCmd.Flags().BoolVar(&flagNotifyReport, "report", false, "Type is report")
+	notifyCmd.MarkFlagsMutuallyExclusive("lgtm", "report")
 }
