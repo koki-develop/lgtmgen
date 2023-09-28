36a33e01295f023911346706f8d1bc51d4c71b1b
koki-develop <kou.pg.0131@gmail.com>
2023-09-28 13:59:09 +0900

    Implement util.Contains

--- backend/internal/util/util.go
+++ backend/internal/util/util.go
@@ -3,3 +3,12 @@ package util
 func Ptr[T any](v T) *T {
 	return &v
 }
+
+func Contains[T comparable](s []T, v T) bool {
+	for _, e := range s {
+		if e == v {
+			return true
+		}
+	}
+	return false
+}
