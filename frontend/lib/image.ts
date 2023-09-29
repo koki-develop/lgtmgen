968418e84e2b5d3b611d17e0743e8a0347d4f4f9
koki-develop <kou.pg.0131@gmail.com>
2023-09-29 21:01:06 +0900

    Implement lgtmUrl()

--- frontend/lib/image.ts
+++ frontend/lib/image.ts
@@ -1,3 +1,7 @@
+export const lgtmUrl = (id: string): string => {
+  return `${process.env.NEXT_PUBLIC_IMAGES_BASE_URL}/${id}`;
+};
+
 export const fileToDataUrl = (file: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
