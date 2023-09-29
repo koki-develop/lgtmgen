be154d3355372c6e52ae68a917dc8cbb1c439785
koki-develop <kou.pg.0131@gmail.com>
2023-09-29 22:28:29 +0900

    Create middleware

---
+++ frontend/middleware.ts
@@ -0,0 +1,32 @@
+import Negotiator from "negotiator";
+import { NextRequest, NextResponse } from "next/server";
+
+const supportedLocales = ["ja", "en"];
+const defaultLocale = "ja";
+
+const extractLocale = (headers: Negotiator.Headers) => {
+  return (
+    new Negotiator({ headers }).language(supportedLocales) ?? defaultLocale
+  );
+};
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = supportedLocales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+  if (pathnameHasLocale) return;
+
+  const headers = {
+    "accept-language": request.headers.get("accept-language") ?? "",
+  };
+  const locale = extractLocale(headers);
+
+  if (locale === defaultLocale) return;
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: ["/((?!_next).*)"],
+};
