aab293c9b9d0fddba5145dec32309e6117006e14
koki-develop <kou.pg.0131@gmail.com>
2023-09-30 08:17:51 +0900

    app/locale/ -> locale/

--- frontend/lib/i18n.ts
+++ frontend/lib/i18n.ts
@@ -1,5 +1,5 @@
-import ja from "@/app/locale/ja";
-import en from "@/app/locale/en";
+import ja from "@/locale/ja";
+import en from "@/locale/en";
 
 export const i18n = (locale: string) => {
   const t = { ja, en }[locale];
