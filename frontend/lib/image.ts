2bc90065da1adacb3f2214b423ae9e5d340cf309
koki-develop <kou.pg.0131@gmail.com>
2023-09-29 18:29:24 +0900

    Implement fileToDataUrl and dataUrlToBase64

---
+++ frontend/lib/image.ts
@@ -0,0 +1,12 @@
+export const fileToDataUrl = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+    reader.readAsDataURL(file);
+  });
+};
+
+export const dataUrlToBase64 = (dataUrl: string): string => {
+  return dataUrl.split(",")[1];
+};
