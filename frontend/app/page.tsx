700be03103d591ea06e309f3ff882bb888fa951b
koki-develop <kou.pg.0131@gmail.com>
2023-09-29 21:10:03 +0900

    Refactor Home function to use Home component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,3 @@
-import { api } from "@/lib/api";
+import Home from "@/components/pages/Home";
 
-export default async function Home() {
-  const { data: lgtms } = await api.v1.lgtmsList({ limit: 20 });
-
-  return (
-    <main>
-      <ul>
-        {lgtms.map((lgtm) => (
-          <li key={lgtm.id}>{lgtm.id}</li>
-        ))}
-      </ul>
-    </main>
-  );
-}
+export default Home;
