7cb5f754a5df5ddd37d6151c12cecfa2169852ea
koki-develop <kou.pg.0131@gmail.com>
2023-09-30 20:37:28 +0900

    Add Toaster component to Providers.tsx

--- frontend/providers/Providers.tsx
+++ frontend/providers/Providers.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import I18nProvider from "./I18nProvider";
+import { Toaster } from "react-hot-toast";
 
 export type ProvidersProps = {
   locale: string;
@@ -9,5 +10,10 @@ export type ProvidersProps = {
 };
 
 export default function Providers({ children, locale }: ProvidersProps) {
-  return <I18nProvider locale={locale}>{children}</I18nProvider>;
+  return (
+    <>
+      <Toaster />
+      <I18nProvider locale={locale}>{children}</I18nProvider>
+    </>
+  );
 }
