1a4d3c0ccf43a44d71f79b8fa12f8de5ce989295
koki-develop <kou.pg.0131@gmail.com>
2023-09-30 09:57:37 +0900

    Add type definition for i18n function

--- frontend/lib/i18n.ts
+++ frontend/lib/i18n.ts
@@ -1,7 +1,14 @@
 import ja from "@/locale/ja";
 import en from "@/locale/en";
 
-export const i18n = (locale: string) => {
+export type I18n = {
+  app: string;
+  lgtm: string;
+  searchImage: string;
+  favorite: string;
+};
+
+export const i18n = (locale: string): I18n => {
   const t = { ja, en }[locale];
   if (!t) throw new Error(`Unsupported locale: ${locale}`);
 
