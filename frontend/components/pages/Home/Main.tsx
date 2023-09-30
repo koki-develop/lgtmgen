085a9a8193ebedb398ad4f8ab986c554026a8d80
koki-develop <kou.pg.0131@gmail.com>
2023-09-30 15:56:27 +0900

    Increase padding in Home page tabs

--- frontend/components/pages/Home/Main.tsx
+++ frontend/components/pages/Home/Main.tsx
@@ -69,7 +69,7 @@ export default function Main({ locale, initialData, perPage }: MainProps) {
             <Tab
               key={label}
               className={clsx(
-                "flex-grow py-2 outline-none transition",
+                "flex-grow py-4 outline-none transition",
                 "border-b-2 border-b-white",
                 "hover:border-b-gray-100 hover:bg-gray-100",
                 "ui-selected:border-b-primary-main ui-selected:font-semibold ui-selected:text-primary-main ui-not-selected:text-gray-400",
