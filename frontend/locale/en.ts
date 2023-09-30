1db5e9eb4aaab55fdaba42946be573f7807ad615
koki-develop <kou.pg.0131@gmail.com>
2023-09-30 09:16:43 +0900

    Add new terms to English locale file

--- frontend/locale/en.ts
+++ frontend/locale/en.ts
@@ -1,3 +1,6 @@
 export default {
   app: "LGTM Generator",
+  lgtm: "LGTM",
+  searchImage: "Search Image",
+  favorite: "Favorite",
 } as const;
--- frontend/locale/ja.ts
+++ frontend/locale/ja.ts
@@ -1,3 +1,6 @@
 export default {
   app: "LGTM Generator",
+  lgtm: "LGTM",
+  searchImage: "画像検索",
+  favorite: "お気に入り",
 } as const;
