dfc291cb78a485b6ab0953976e18c84463f4acec
koki-develop <kou.pg.0131@gmail.com>
2023-10-04 21:42:09 +0900

    Implement resizeDataUrl

--- frontend/lib/image.ts
+++ frontend/lib/image.ts
@@ -14,3 +14,36 @@ export const fileToDataUrl = (file: File): Promise<string> => {
 export const dataUrlToBase64 = (dataUrl: string): string => {
   return dataUrl.split(",")[1];
 };
+
+export const resizeDataUrl = async (
+  dataUrl: string,
+  sideLength: number,
+  type: "image/jpeg" | "image/png",
+): Promise<string> => {
+  // load image
+  const image = new Image();
+  image.src = dataUrl;
+  await new Promise<void>((resolve) => {
+    image.onload = () => resolve();
+  });
+
+  // create canvas
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  if (!ctx) {
+    throw new Error("Failed to get canvas context");
+  }
+
+  // keep aspect ratio
+  const ratio = Math.max(sideLength / image.width, sideLength / image.height);
+  const width = image.width * ratio;
+  const height = image.height * ratio;
+
+  // resize
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(image, 0, 0, width, height);
+
+  // convert to data url
+  return canvas.toDataURL(type);
+};
