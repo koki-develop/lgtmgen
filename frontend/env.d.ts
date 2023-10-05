e4dbf62abe3ff0f4a312da181b25955897d4a459
koki-develop <kou.pg.0131@gmail.com>
2023-10-05 18:54:40 +0900

    Add environment variables

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,7 +1,9 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    readonly NEXT_PUBLIC_STAGE: string;
     readonly NEXT_PUBLIC_API_BASE_URL: string;
     readonly NEXT_PUBLIC_IMAGES_BASE_URL: string;
+    readonly NEXT_PUBLIC_GA_MEASUREMENT_ID: string;
     // Sentry
     readonly NEXT_PUBLIC_SENTRY_ORG: string;
     readonly NEXT_PUBLIC_SENTRY_PROJECT: string;
