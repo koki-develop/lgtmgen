a3c86ff3beb1a87e0b575de99853050461594a9b
koki-develop <kou.pg.0131@gmail.com>
2023-10-06 06:07:21 +0900

    Create aws resources for prd

---
+++ terraform/environments/prd/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  stage = "prd"
+  name  = "lgtmgen-${local.stage}"
+}
---
+++ terraform/environments/prd/main.tf
@@ -0,0 +1,24 @@
+module "github_actions" {
+  source = "../../modules/aws/github_actions"
+  name   = local.name
+}
+
+module "s3_images" {
+  source           = "../../modules/aws/s3"
+  name             = local.name
+  tier             = "images"
+  distribution_arn = module.cloudfront_images.this.arn
+}
+
+module "s3_news" {
+  source = "../../modules/aws/s3"
+  name   = local.name
+  tier   = "news"
+}
+
+module "cloudfront_images" {
+  source                    = "../../modules/aws/cloudfront"
+  name                      = local.name
+  tier                      = "images"
+  origin_bucket_domain_name = module.s3_images.this.bucket_regional_domain_name
+}
