5e00501f321d3849a4f909fc196a032970b798f3
koki-develop <kou.pg.0131@gmail.com>
2023-10-07 21:15:59 +0900

    Setup sentry

--- backend/cmd/root.go
+++ backend/cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/cockroachdb/errors"
 	"github.com/koki-develop/lgtmgen/backend/internal/env"
+	"github.com/koki-develop/lgtmgen/backend/internal/sentry"
 	"github.com/spf13/cobra"
 )
 
@@ -13,6 +14,11 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() error {
+	if err := sentry.Setup(); err != nil {
+		return errors.Wrap(err, "failed to setup sentry")
+	}
+	defer sentry.Flush()
+
 	if err := env.Load(); err != nil {
 		return errors.Wrap(err, "failed to load env")
 	}
