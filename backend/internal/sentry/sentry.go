92249896406f9b9e9f54667e7b8df880327ba8aa
koki-develop <kou.pg.0131@gmail.com>
2023-10-07 21:11:54 +0900

    Implement sentry.Flush

--- backend/internal/sentry/sentry.go
+++ backend/internal/sentry/sentry.go
@@ -1,17 +1,19 @@
 package sentry
 
 import (
+	"time"
+
 	"github.com/cockroachdb/errors"
 	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-gonic/gin"
 	"github.com/koki-develop/lgtmgen/backend/internal/env"
+	"github.com/koki-develop/lgtmgen/backend/internal/log"
 )
 
 func Setup() error {
-	if disabled() {
-		return nil
-	}
-
 	err := sentry.Init(sentry.ClientOptions{
+		Debug:            debug(),
 		Dsn:              env.Vars.SentryDSN,
 		EnableTracing:    true,
 		TracesSampleRate: 1.0,
@@ -23,6 +25,19 @@ func Setup() error {
 	return nil
 }
 
-func disabled() bool {
+func Flush() {
+	sentry.Flush(10 * time.Second)
+}
+
+func CaptureExceptionWithGin(ctx *gin.Context, err error) {
+	hub := sentrygin.GetHubFromContext(ctx)
+	if hub == nil {
+		log.Warn(ctx, "failed to get sentry hub from context")
+		return
+	}
+	_ = hub.CaptureException(err)
+}
+
+func debug() bool {
 	return env.Vars.Stage == "local"
 }
