c048620a2223258f6bac55dcf8d14b9839a504a9
koki-develop <kou.pg.0131@gmail.com>
2024-05-01 19:25:17 +0900

    Comment out non-news API calls in Home component

--- frontend/components/pages/Home/Home.tsx
+++ frontend/components/pages/Home/Home.tsx
@@ -10,35 +10,35 @@ export type HomeProps = {
 };
 
 export default async function Home({ params: { locale } }: HomeProps) {
-  const lang = ["ja", "en"].includes(locale) ? locale : "ja";
+  // const lang = ["ja", "en"].includes(locale) ? locale : "ja";
   const newsListing = api.v1.newsList({ locale });
-  const lgtmsListing = api.v1.lgtmsList({ limit: perPage });
-  const lgtmsRandomListing = api.v1.lgtmsList({ limit: perPage, random: true });
-  const categoriesListing = api.v1.categoriesList({ lang });
+  // const lgtmsListing = api.v1.lgtmsList({ limit: perPage });
+  // const lgtmsRandomListing = api.v1.lgtmsList({ limit: perPage, random: true });
+  // const categoriesListing = api.v1.categoriesList({ lang });
 
   const [
     newsListResponse,
-    lgtmsListResponse,
-    lgtmListRandomResponse,
-    categoriesListResponse,
+    // lgtmsListResponse,
+    // lgtmListRandomResponse,
+    // categoriesListResponse,
   ] = await Promise.all([
     newsListing,
-    lgtmsListing,
-    lgtmsRandomListing,
-    categoriesListing,
+    // lgtmsListing,
+    // lgtmsRandomListing,
+    // categoriesListing,
   ]);
   if (!newsListResponse.ok) throw newsListResponse.error;
-  if (!lgtmsListResponse.ok) throw lgtmsListResponse.error;
-  if (!lgtmListRandomResponse.ok) throw lgtmListRandomResponse.error;
-  if (!categoriesListResponse.ok) throw categoriesListResponse.error;
+  // if (!lgtmsListResponse.ok) throw lgtmsListResponse.error;
+  // if (!lgtmListRandomResponse.ok) throw lgtmListRandomResponse.error;
+  // if (!categoriesListResponse.ok) throw categoriesListResponse.error;
 
   return (
     <Main
       locale={locale}
       newsList={newsListResponse.data}
-      initialData={lgtmsListResponse.data}
-      initialRandomData={lgtmListRandomResponse.data}
-      categories={categoriesListResponse.data}
+      initialData={[]}
+      initialRandomData={[]}
+      categories={[]}
       perPage={perPage}
     />
   );
